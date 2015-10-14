3b930e04064e0f26c1962fc3a8d4522726b0f1ee
Louis Gill <lgill7@cnm.edu>
2015-10-14 15:56:45 -0600

    persona

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <h2>Persona:</h2>
     <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects. Because of the heavy use of numbers, this user is probably an adult. The user is someone with highly disposable time and a competitive nature.</p>
     <h2>Use case:</h2>
-    <p></p>
+    <p>From the get-go, the user can see the name of their cineplex along the top of the screen. Underneath this is an auxillary bar which informs them of two things:</p>
+    <ul>
+        <li>The deadline by which they must have their picks locked in (date/time)</li>
+        <li>The amount of "FML bucks" they have remaining in their account</li>
+    </ul>
+    <p>In the main portion of the page, </p>
     </body>
 </html>
