fb1d22142a5ea73b94a8218ff7b3b48d4f4ae5a2
Louis Gill <lgill7@cnm.edu>
2015-10-14 15:43:07 -0600

    persona

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
     <h1>Persona:</h1>
-    <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects.</p>
+    <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects. Because of the heavy use of numbers, this user is probably an adult.</p>
     <h1>Use case:</h1>
     <p></p>
     </body>
