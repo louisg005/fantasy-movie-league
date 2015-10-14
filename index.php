ea6868d5b4b818a4417b505f4ccf34abc850ce9d
Louis Gill <lgill7@cnm.edu>
2015-10-14 16:46:14 -0600

    screen slots

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <h1>Fantasy Movie League</h1>
     <hr>
     <h2>Persona:</h2>
-    <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects. Because of the heavy use of numbers, this user is probably an adult. The user is someone with highly disposable time and a competitive nature.</p>
+    <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects. This user is probably an adult. The user is someone with highly disposable time and a competitive nature.</p>
     <h2>Use case:</h2>
     <p>The user's primary objective is to fill the screens of their "cineplex" with the movies of their choice, while remaining under budget.</p>
     <p>From the get-go, the user can see the name of their cineplex along the top of the screen. Underneath this is an auxillary bar which informs them of two things:</p>
@@ -17,6 +17,6 @@
         <li>The amount of "FML bucks" they have remaining in their account</li>
     </ul>
     <p>In the main body of the page, the user can view the eight empty "theater screens" alongside a list of the available movies.<br><br>Each item in the movie list is represented by an image from the film, the title of the film, and its cost to the user (in FB$).<br><br>The first action taken by the user is to select a movie. By clicking on one of the listed movies, the user has selected that movie to play on "Screen 1". This is represented to the user by the appearance of the movie's poster in the Screen 1 box. Taking this action also subtracts the cost of the movie from the user's total available FB$, which is reflected in the top portion of the page. If the user's amount of remaining FB$ is less than the cost of any of the movies, then the images representing those movies will become blurry and a line of text will appear over them indicating that they are now "UNAVAILABLE" to the user.</p>
-    <p>The user may wish to revoke their decision to select a movie. To do so, they need only to hover over the chosen movie poster in its Screen Slot. This creates a clickable message, "REMOVE". If the user clicks this, then that movie will be removed the screen. The movie placed in the Screen Slot with the next highest number will then appear in the recently vacated Screen Slot. </p>
+    <p>The user may wish to revoke their decision to select a movie. To do so, they need only to hover over the chosen movie poster in its Screen Slot. This creates a clickable message, "REMOVE". If the user clicks this, then that movie will be removed from the screen. Any movies located in Screen Slots with higher Screen Numbers than the recently vacated slot will move down to fill the lowest possible Screen Slot Numbers. </p>
     </body>
 </html>
