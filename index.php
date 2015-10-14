3124b1ca020fd818294b3106edd6335b1e8bd32a
Louis Gill <lgill7@cnm.edu>
2015-10-14 15:22:06 -0600

    initialization

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Fantasy Movie League</title>
+    </head>
+    <body>
+    <p>blah blah blah</p>
+    </body>
+</html>
