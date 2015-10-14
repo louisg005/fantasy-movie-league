98565362d8f24492fd942952a07c849c82b87c21
Louis Gill <lgill7@cnm.edu>
2015-10-14 16:03:25 -0600

    theater screens intro

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
     <h2>Persona:</h2>
     <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects. Because of the heavy use of numbers, this user is probably an adult. The user is someone with highly disposable time and a competitive nature.</p>
     <h2>Use case:</h2>
-    <p>The user's primary objective is to fill the screens of their "cineplex" with the movies of their choice, but remaining under budget.</p>
+    <p>The user's primary objective is to fill the screens of their "cineplex" with the movies of their choice, while remaining under budget.</p>
     <p>From the get-go, the user can see the name of their cineplex along the top of the screen. Underneath this is an auxillary bar which informs them of two things:</p>
     <ul>
         <li>The deadline by which they must have their picks locked in (date/time)</li>
         <li>The amount of "FML bucks" they have remaining in their account</li>
     </ul>
-    <p>In the main portion of the page, </p>
+    <p>In the main body of the page, the user can view the eight empty "theater screens" alongside a list of the available movies.</p>
     </body>
 </html>
