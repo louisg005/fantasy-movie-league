977ebda2b90b653758ccc195c1acb6def9335cc9
Louis Gill <lgill7@cnm.edu>
2015-10-15 08:24:06 -0600

    Jesse

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <h1>Fantasy Movie League</h1>
     <hr>
     <h2>Persona:</h2>
-    <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects. This user is probably an adult. The user was probably attracted to the site to compete with friends and family in a battle of wits, and thus does not necessarily have an overabundance of technological ability. Due to the simple and quick nature of the site, it is likely that the user would be interacting with it in passing on a mobile device.</p>
+    <p>Jesse is using Fantasy Movie Leage. Jesse is a cinephile. Not merely a movie buff, Jesse has an eccentricity for the internal workings of the film industry, particularly the financial aspects. Jesse is 18-40 years old and male. Jesse was attracted to the site to compete with friends and family in a battle of wits, and thus does not have an overabundance of technological ability. Due to the simple and quick nature of the site, Jesse interacts with it in passing on a mobile device.</p>
     <h2>Use case:</h2>
-    <p>The user's objective on this page is to fill the screens of their "cineplex" with the movies of their choice, while remaining under budget. Their ultimate goal is to accumulate more "box office wealth" than those they are playing against.</p>
-    <p>From the get-go, the user can see the name of their cineplex along the top of the screen. Adjacent to this are meters indicating the user's total box office commissions and where that ranks among other users during the current season. Directly below this is a bar which informs the user of 4 things:</p>
+    <p>Jesse's objective on this page is to fill the screens of his "cineplex" with the movies of his choice, while remaining under budget. His ultimate goal is to accumulate more "box office wealth" than those he is playing against.</p>
+    <p>From the get-go, the system displays Jesse's name along the top of the screen. Adjacent to this are meters indicating the Jesse's total box office commissions and where that ranks among other users during the current season. Directly below this is a bar which informs the user of 4 things:</p>
     <ul>
         <li>The current season of play</li>
         <li>The current week within the season</li>
-        <li>The deadline by which they must have their picks locked in (date/time)</li>
+        <li>The deadline by which Jesse must have his picks locked in (date/time)</li>
         <li>The amount of "FB$" they have remaining in their account</li>
     </ul>
     <p>In the main body of the page, the user can view the eight empty "theater screens" alongside a list of the available movies.<br><br>Each item in the movie list is represented by an image from the film, the title of the film, and its cost to the user (in FB$).<br><br>The first action taken by the user is to select a movie. By clicking on one of the listed movies, the user has selected that movie to play on "Screen 1". This is represented to the user by the appearance of the movie's poster in the Screen 1 box. Taking this action also subtracts the cost of the movie from the user's total available FB$, which is reflected in the top portion of the page. If the user's amount of remaining FB$ is less than the cost of any of the movies, then the images representing those movies will become blurry and a line of text will appear over them indicating that they are now "UNAVAILABLE" to the user.</p>
@@ -28,4 +28,11 @@
         <li>All 8 of the Screen Slots have been filled</li>
     </ul>
     <p><strong>IMPORTANT NOTE:</strong>  The user is not required to spend all of their allocated FB$ or fill all 8 Screen Slots, nor may it be in their best interest to do so strategically. Because of this, the user's entries are constantly saved automatically, but the indication of this is small and could easily go unnoticed. A "confirm changes" button, while not technically necessary, could lead to greater user satisfaction and understanding of the rules of the game.</p>
+    <hr>
+    <p>USER:</p>
+    <ul>
+        <li>name</li>
+        <li>total box office</li>
+        <li>season ranking</li>
+
 </html>
