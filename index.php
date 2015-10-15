d47440477b49bbbdada335332bddf9cce5850893
Louis Gill <lgill7@cnm.edu>
2015-10-14 20:16:44 -0600

    top bar

--- index.php
+++ index.php
@@ -10,16 +10,18 @@
     <h2>Persona:</h2>
     <p>The core user of Fantasy Movie League is a cinephile. Not merely a movie buff, this person has an eccentricity for the internal workings of the film industry, particularly the financial aspects. This user is probably an adult. The user was probably attracted to the site to compete with friends and family in a battle of wits, and thus does not necessarily have an overabundance of technological ability. Due to the simple and quick nature of the site, it is likely that the user would be interacting with it in passing on a mobile device.</p>
     <h2>Use case:</h2>
-    <p>The user's primary objective is to fill the screens of their "cineplex" with the movies of their choice, while remaining under budget.</p>
-    <p>From the get-go, the user can see the name of their cineplex along the top of the screen. Underneath this is an auxillary bar which informs them of two things:</p>
+    <p>The user's objective on this page is to fill the screens of their "cineplex" with the movies of their choice, while remaining under budget. Their ultimate goal is to accumulate more "box office wealth" than those they are playing against.</p>
+    <p>From the get-go, the user can see the name of their cineplex along the top of the screen. Adjacent to this are meters indicating the user's total box office commissions and where that ranks among other users during the current season. Directly below this is a bar which informs the user of 4 things:</p>
     <ul>
+        <li>The current season of play</li>
+        <li>The current week within the season</li>
         <li>The deadline by which they must have their picks locked in (date/time)</li>
         <li>The amount of "FML bucks" they have remaining in their account</li>
     </ul>
     <p>In the main body of the page, the user can view the eight empty "theater screens" alongside a list of the available movies.<br><br>Each item in the movie list is represented by an image from the film, the title of the film, and its cost to the user (in FB$).<br><br>The first action taken by the user is to select a movie. By clicking on one of the listed movies, the user has selected that movie to play on "Screen 1". This is represented to the user by the appearance of the movie's poster in the Screen 1 box. Taking this action also subtracts the cost of the movie from the user's total available FB$, which is reflected in the top portion of the page. If the user's amount of remaining FB$ is less than the cost of any of the movies, then the images representing those movies will become blurry and a line of text will appear over them indicating that they are now "UNAVAILABLE" to the user.</p>
     <p>The user may wish to revoke their decision to select a movie. To do so, they need only to hover over the chosen movie poster in its Screen Slot. This creates a clickable message, "REMOVE". If the user clicks this, then that movie will be removed from the screen. Any movies located in Screen Slots with higher Screen Numbers than the recently vacated slot will each move down by one slot to fill the lowest possible Screen Slot Numbers. </p>
     <p>Each time the user adds or removes a movie from their Cineplex, a message briefly appears near the top of the screen notifying them that "ALL CHANGES (HAVE BEEN) SAVED"</p>
-    <p>The user continues to select available movies from the list until one of 2 things happen:</p>
+    <p>The user may continue to select available movies from the list until one of 2 things happen:</p>
     <ul>
         <li>Their remaining FB$ is no longer equal to or greater than the cost of any movie</li>
         <li>All 8 of the Screen Slots have been filled</li>
