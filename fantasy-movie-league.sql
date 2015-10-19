dd63edc2ef498c1403555bbde991493f4470eae4
Louis Gill <lgill7@cnm.edu>
2015-10-19 13:01:52 -0600

    movies data

--- fantasy-movie-league.sql
+++ fantasy-movie-league.sql
@@ -24,4 +24,31 @@ CREATE TABLE screens (
 
 INSERT INTO movies (movieTitle,buxCost)
     VALUES("The Martian", 410);
-
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Goosebumps", 369);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Bridge of Spies", 336);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Crimson Peak", 311);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Hotel Transylvania 2", 225);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Pan (2015)", 144);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("The Intern", 102);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Sicario", 77);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Woodlawn",74);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Maze Runner: The Scorch Trials", 60);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("The Walk", 45);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Everest", 30);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("Black Mass", 29);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("The Visit", 28);
+INSERT INTO movies (movieTitle,buxCost)
+    VALUES("War Room", 24);
